df262c503da1b12731cbc6d688fadd1f823c4422
Thomas White <thomasawhite321@gmail.com>
2026-05-20 12:45:43 +0100

    Sentry added to dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import * as Sentry from '@sentry/react';
 import App from './App';
+
+Sentry.init({
+  dsn: 'https://bdde764e9756f8bde7e4ce053ea5c6a5@o4511253268791296.ingest.de.sentry.io/4511253280915536',
+  environment: 'production',
+  integrations: [],
+  tracesSampleRate: 0.05,
+  replaysSessionSampleRate: 0.01,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
